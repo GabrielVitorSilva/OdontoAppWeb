1daacece3575a5e98e059c1131b0cc23c5da6db6
GabrielVitor08 <gabrieldatas2004@gmail.com>
2025-06-17 12:15:44 -0300

    Adiciona a propriedade suppressHydrationWarning ao corpo do layout, melhorando a renderização em ambientes de hidratação no Next.js. Remove uma linha em branco desnecessária no início do arquivo.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
@@ -22,7 +21,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         <AuthProvider>
           {children}
           <Toaster />
